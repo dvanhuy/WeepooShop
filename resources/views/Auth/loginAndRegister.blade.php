e51a0ba7fd903351826758dc40caf0f3446e0a45
Vanhuy <dinhvanhuy13@gmail.com>
2023-11-01 19:51:11 +0700

    laravel mix

--- resources/views/Auth/loginAndRegister.blade.php
+++ resources/views/Auth/loginAndRegister.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
-    <link rel="stylesheet" href="{{ asset('css/app.css')}}">
+    <link rel="stylesheet" href="{{ asset('css/auth.css')}}">
     <title>Weepoo Shop Login</title>
 </head>
 
